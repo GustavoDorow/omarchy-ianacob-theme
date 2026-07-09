df2ac659d48651c79e65a027e35a9ea63d6325b1
HANCORE <m.fattah-ahmad@gmx.de>
2026-07-09 18:18:10 +0200

    Add files via upload

--- neovim.lua
+++ neovim.lua
@@ -1,35 +1,63 @@
 return {
   {
     "bjarneo/aether.nvim",
-    branch = "v2",
+    branch = "v3",
     name = "aether",
     priority = 1000,
     opts = {
       transparent = false,
       colors = {
-  bg           = "#0c0b0c",
-  bg_dark      = "#0c0b0c",
-  bg_highlight = "#716661",     -- still only change to bg_highlight
+        bg         = "#05080a",
+        dark_bg    = "#05080a",
+        darker_bg  = "#0A0D0F",
+        lighter_bg = "#28213A",
 
-  -- Foregrounds
-  fg           = "#f1f1ef",
-  fg_dark      = "#e0dbd9",
-  comment      = "#94918c",
+        fg         = "#f3ceff",
+        dark_fg    = "#ffe1e1",
+        light_fg   = "#ffecff",
+        bright_fg  = "#fff6ff",
+        muted      = "#9094A2",
 
-  red          = "#e36a58",
-  orange       = "#c38d72",
-  yellow       = "#c8a88a",
-  green        = "#a4dded",
-  cyan         = "#725379",
-  blue         = "#4691a1",
-  purple       = "#c9a3c9",
-  magenta      = "#b9a0d1",
+        red        = "#ff9fbc",
+        orange     = "#ffc79b",
+        yellow     = "#fff6c3",
+        green      = "#baf7b5",
+        cyan       = "#b2fff3",
+        blue       = "#9ab4f9",
+        purple     = "#dfbaff",
+        brown      = "#b572ef",
+
+        bright_red    = "#ffaecb",
+        bright_yellow = "#fff6c3",
+        bright_green  = "#baf7b5",
+        bright_cyan   = "#b2fff3",
+        bright_blue   = "#9ab4f9",
+        bright_purple = "#dfbaff",
+
+        accent               = "#b2fff3",
+        cursor               = "#b2fff3",
+        foreground           = "#fff6ff",
+        background           = "#05080a",
+        selection             = "#28213A",
+        selection_foreground = "#05080a",
+        selection_background = "#ffc2df",
       },
+      on_highlights = function(hl, c)
+        hl.CursorLine = { bg = c.lighter_bg }
+        hl.CursorLineNr = { fg = c.yellow, bold = true }
+        hl.Visual = { bg = c.selection_background, fg = c.selection_foreground }
+        hl.LspReferenceText = { bg = c.selection, fg = c.bright_fg }
+        hl.LspReferenceRead = hl.LspReferenceText
+        hl.LspReferenceWrite = hl.LspReferenceText
+        hl.SnacksPickerDir         = { fg = c.muted }
+        hl.SnacksPickerPathHidden  = { fg = c.muted }
+        hl.SnacksPickerPathIgnored = { fg = c.muted }
+        hl.SnacksPickerListCursorLine = { bg = c.lighter_bg }
+      end,
     },
     config = function(_, opts)
       require("aether").setup(opts)
       vim.cmd.colorscheme("aether")
-      require("aether.hotreload").setup()
     end,
   },
   {
